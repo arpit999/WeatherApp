fe164a747e5edd4213b5ba0ed82e7c357e6c6ff6
arpit <arpitpatel009@gmail.com>
2024-05-20 22:20:00 -0400

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Arpit & Jinal on 2024-05-20.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
